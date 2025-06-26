e27a9de37d3679537a3de976ff53853de0c45807
soknapich <soknait@gmail.com>
2025-06-26 23:10:04 +0700

    remove logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <h1>Hello Jenkins,​តើមានអ្វីអោយខ្ញុំជួយបានទេ? </h1>
         <a
           className="App-link"
