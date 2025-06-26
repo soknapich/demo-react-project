8ae1e71faa66e8ceb9bea86302bbc821070660a8
soknapich <soknait@gmail.com>
2025-06-26 22:38:04 +0700

    Hello Jenkins

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello React js
-        </p>
+        <h1>Hello Jenkins </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
